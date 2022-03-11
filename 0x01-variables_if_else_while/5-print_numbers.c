06158de0186eccd54f3408903fef75063fd5f79b
Juma-h <mitchfibi@gmail.com>
2022-03-11 22:00:05 +0300

    count num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function 
+ * Description: number count
+ * Return: 0
+ *
+*/
+int main(void) 
+{
+	int count;
+
+
+	for(count = 0; count < 10; count++)
+		printf("%d ", count);
+
+	printf("\n");
+	return (0);
+
+}
