fd08aa4c4f6bf69b7bee7aca334ec035d39296f5
Juma-h <mitchfibi@gmail.com>
2022-03-15 23:36:04 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,15 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-
-	if (n < 0)
-		n = -n;
+	if (n > 0)
+	{
+		n = n % 10;
 		_putchar(n + '0');
-		return (n);
+	}
+	else
+	{
+		n = n % 10 * -1;
+		_putchar(n + '0');
+	}
+	return (n);
 }
