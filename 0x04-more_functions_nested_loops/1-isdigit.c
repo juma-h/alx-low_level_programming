28b2b24b901c2a2f01c8c6497a86fb5add2cb621
Juma-h <mitchfibi@gmail.com>
2022-03-17 20:25:19 +0300

    check if integer

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,18 +8,18 @@
 int _isdigit(int c)
 {
 	char i = '0';
-	int isdigit = 0;
+	int c = 0;
 
 	for (; i <= '9'; i++)
 	{
 		if (i == c)
 		{
-			isdigit = 1;
+			c = 1;
 			break;
 		}
 	}
 
-	return (isdigit);
+	return (c;
 }
 © 2022 GitHub, Inc.
 
