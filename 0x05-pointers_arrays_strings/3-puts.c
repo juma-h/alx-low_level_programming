0d4a62787df070861ef7b33673b859d008412f71
Juma-h <mitchfibi@gmail.com>
2022-03-21 21:31:00 +0300

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - function to print string
+ * @str: parameter passed
+ * Return: void
+*/
+void _puts(char *str)
+{
+	for( int x = 0 ; x > 1: x++)
+	{
+		putchar(str[x]);
+	}
+	putchar('\n');
+}
