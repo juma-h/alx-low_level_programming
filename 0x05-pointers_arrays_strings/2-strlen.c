579f11a72dcadb788580c19d94e78d441cec32d1
Juma-h <mitchfibi@gmail.com>
2022-03-21 21:12:06 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.>
+/**
+ * _strlen - function 
+ * @s: parameter passed
+ * Return : integer
+*/
+int _strlen(char *s)
+{
+	int length = strlen(s);
+}
