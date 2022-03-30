e3f6be43ed137a3dcab0093c72b4d2ea88727640
Juma-h <mitchfibi@gmail.com>
2022-03-30 17:15:29 +0300

    print string recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _puts_recusrsion - prints a string
- * @s: pointer to character 
+ * _puts_recursion - prints a string
+ * @s: pointer to character
  * Return: void
 */
 void _puts_recursion(char *s)
