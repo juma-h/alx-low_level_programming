91d438cb9c0806d95b53c12df888b8cfbcb40b51
Juma-h <mitchfibi@gmail.com>
2022-03-30 22:18:27 +0300

    i dont understand this

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sprt_recursion - return square root of number
+ * @n: integer of num
+ * Return: if not natural square root, -1
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
