7d30894a0c838b1835ba65e47fd9548e76b98bdf
Juma-h <mitchfibi@gmail.com>
2022-04-01 16:11:01 +0300

    product of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - function that gets the product of two numbers
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: 0 or 1
+*/
+int main(int argc, char *arhv[])
+{
+	int x;
+	int y;
+	int product;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		product = x * y;
+		printf("%d\n", product);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
