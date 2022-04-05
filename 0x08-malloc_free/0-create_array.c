1d75c3c8660795442b815acd0581d51c868a7468
Juma-h <mitchfibi@gmail.com>
2022-04-05 21:11:53 +0300

    array of chars

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.>
+#include "main.h"
+/**
+ * create_array - create array of char
+ * @size: array size
+ * @c: storage character
+ * Return: pointer of arr of chars
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(sizeof(c) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+}
