e01acc0dc2845ec398ce4f18c1b0e2fd84bd1311
Juma-h <mitchfibi@gmail.com>
2022-04-13 19:38:47 +0300

    execute function through array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array || action == NULL)
+	if (array & action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
