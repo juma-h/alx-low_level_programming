7bec2a20bb2e59ffe9de8d845b1f54e46f72c74e
Juma-h <mitchfibi@gmail.com>
2022-04-21 22:16:04 +0300

    attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void myPrint(void) __attribute__ ((destructor));
+
+/**
+ * myPrint - prints before main function
+ * Return: no return
+*/
+
+void myPrint(void)
+{
+	printf ("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
