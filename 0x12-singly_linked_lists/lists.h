726b349422f05a48afd6e1cf26fad65283b20ba7
Juma-h <mitchfibi@gmail.com>
2022-04-21 21:35:37 +0300

    header changed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,15 +11,14 @@
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 
 size_t print_list(const list_t *h);
 
