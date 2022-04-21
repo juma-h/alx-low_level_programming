3d4aa8777782f631dfb0a3bd13f1d884a9c444fc
Juma-h <mitchfibi@gmail.com>
2022-04-21 22:21:46 +0300

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void myPrint (void) __attribute__ ((constructor));
+void bprint(void) __attribute__ ((constructor));
 
 /**
- * myPrint - prints before main function
+ * bprint - prints a sentence before the main
  * Return: no return
-*/
+ */
 
-void myPrint(void)
+void bprint(void)
 {
-	printf ("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
