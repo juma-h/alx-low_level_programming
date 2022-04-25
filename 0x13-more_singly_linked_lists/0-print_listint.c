34005494035123bfcae1ebebfb0c695f75a8c629
Juma-h <mitchfibi@gmail.com>
2022-04-25 20:48:42 +0300

    print change

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 
 /**
- * listint_len - print elements of linkedlist
+ * print_listint - print elements of linkedlist
  * @h: linked list
  *
  * Return: number of nodes
  */
 
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t nod = 0;
+	size_t num = 0;
 	const listint_t *current = h;
 
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
 		current = current->next;
-		nod++;
+		num++;
 	}
-
-	return (nod);
+	return (num);
 }
